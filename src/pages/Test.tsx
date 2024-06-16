441d396de9a2f087760d509dfa1dac7c47d9521a
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-06-16 03:55:55 +0200

    Create dropdown menu component

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,10 +1,35 @@
-import {FC} from "react"
+import { FC } from "react";
+import DropdownMenu from "../components/dropdown/Dropdown";
+import { useNavigate } from "react-router-dom";
 
+const Test: FC = () => {
+  const navigate = useNavigate();
+  const dropdownTestData = [
+    {
+      id: "1",
+      onClick: () => console.log("id 1"),
+      content: <div>Item 1</div>,
+    },
+    {
+      id: "2",
+      onClick: () => console.log("id 2"),
+      content: "Item 2",
+    },
+    {
+      id: "3",
+      onClick: () => navigate("/"),
+      content: <div>Navigate Home</div>,
+    },
+  ];
 
-const Test:FC = () => {
-    return(
-        <h1>Test page</h1>
-    )
-}
+  return (
+    <div style={{ padding: "32px" }}>
+      <h1>Test page</h1>
+      <div style={{ width: "200px" }}>
+        <DropdownMenu data={dropdownTestData} title="Test Dropdown" />
+      </div>
+    </div>
+  );
+};
 
-export default Test
+export default Test;
