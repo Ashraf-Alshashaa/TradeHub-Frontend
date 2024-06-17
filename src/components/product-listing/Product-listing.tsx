ff5906cf3fb71db57a1b444dbbfe6db6064aaa40
Ladan <ladan.rb@gmail.com>
2024-06-17 17:28:43 +0200

    Initiate the product item listing component.

---
+++ src/components/product-listing/Product-listing.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './product-listing.css';
+import { Product } from './types';
+
+interface ProductListingProps {
+    product: Product;
+  }
+  
+  const ProductListing: React.FC<ProductListingProps> = ({ product }) => {
+    return (
+      <div className="card mb-3">
+        <div className="row g-0">
+            <div className="col-md-4">
+                <img src={product.image} className="img-fluid rounded-start" alt={product.name} />
+            </div>
+            <div className="col-md-4">
+                <div className="card-body justify-content-center">
+                    <h5 className="my-2">{product.name}</h5>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div className="card-body justify-content-center">
+                    <p className="card-text my-2">{product.price}</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+  };
+  
+  export default ProductListing;
