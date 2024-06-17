246a32e670ae28dd7bf7459b3d2953b2aa432831
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-06-17 11:46:49 +0200

    Removed styles from tsx file

--- src/components/product-card/Product-card.tsx
+++ src/components/product-card/Product-card.tsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import './product-card.css';
-import {ProductCardProps} from './types'
-import Card from 'react-bootstrap/Card';
-import { CardImg, CardTitle } from 'react-bootstrap';
-import { ListGroup } from 'react-bootstrap';
+import React from "react";
+import "./product-card.css";
+import { ProductCardProps } from "./types";
+import Card from "react-bootstrap/Card";
+import { CardImg, CardTitle } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
 
-
-const ProductCard: React.FC<ProductCardProps> = ({ photo, name, price, location }) => {
-
-const CardClick = () => {
-  console.log('Card clicked!');
-};
-    return (
+const ProductCard: React.FC<ProductCardProps> = ({
+  photo,
+  name,
+  price,
+  location,
+}) => {
+  const CardClick = () => {
+    console.log("Card clicked!");
+  };
+  return (
     <div onClick={CardClick}>
-    <Card style={{ width: '16rem', height: '18rem' }}>
-      <CardImg variant= 'top' src={photo}/>
-      <Card.Body>
-        <CardTitle> {name} </CardTitle>
-        <ListGroup.Item className='text-success font-weight-bold'>{price}€</ListGroup.Item>
-        <ListGroup.Item>{location}</ListGroup.Item>
-      </Card.Body>
+      <Card>
+        <CardImg variant="top" src={photo} />
+        <Card.Body>
+          <CardTitle> {name} </CardTitle>
+          <ListGroup.Item className="text-success font-weight-bold">
+            {price}€
+          </ListGroup.Item>
+          <ListGroup.Item>{location}</ListGroup.Item>
+        </Card.Body>
       </Card>
-      </div>
-      );
-    };
+    </div>
+  );
+};
 
-  export default ProductCard;
+export default ProductCard;
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,27 +1,27 @@
-import { FC } from 'react';
-import ProductCard from '../components/product-card/Product-card';
-import CustomImage from '../components/image/Image';
+import { FC } from "react";
+import ProductCard from "../components/product-card/Product-card";
+import CustomImage from "../components/image/Image";
 
-const Test: FC = () => {  
-  return(
+const Test: FC = () => {
+  return (
     <div>
-    <h1>Test Page</h1>
-    <div>
-          <ProductCard
-            photo='https://www.helium10.com/app/uploads/2023/08/shutterstock_2251573229-copy-958x632.webp'
-            name='Hair Oil'
-            price={11.40}
-            location='Nijmegen'
-          />
+      <h1>Test Page</h1>
+      <div style={{ width: "16rem", height: "18rem" }}>
+        <ProductCard
+          photo="https://www.helium10.com/app/uploads/2023/08/shutterstock_2251573229-copy-958x632.webp"
+          name="Hair Oil"
+          price={11.4}
+          location="Nijmegen"
+        />
       </div>
-    <CustomImage
-      src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg"
-      alt = ""
-      width={300}
-      height={300}
-    />
+      <CustomImage
+        src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg"
+        alt=""
+        width={300}
+        height={300}
+      />
     </div>
-  ) 
+  );
 };
 
 export default Test;
