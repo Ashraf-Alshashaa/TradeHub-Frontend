e27b689564e26b43cb881b00987e8439207eb481
Ladan <ladan.rb@gmail.com>
2024-06-19 22:29:45 +0200

    Products in the page are clickable now.

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -5,18 +5,17 @@ import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import ProductCard from '../../components/product-card/Product-card';
 import './products.css';
-import { ProductCardProps } from '../../components/product-card/types';
 
 const Test: FC = () => {
   const handlePriceChange = (value: [number, number]) => {
     console.log('Price range changed:', value);
   };
-
+  const cardClicked = () => alert("Card clicked")
   const categories = ['Electronics', 'Furniture', 'Toys', 'Clothes'];
 
   // Example product data
   const products = [
-    { id: 1, photo: "https://www.helium10.com/app/uploads/2023/08/shutterstock_2251573229-copy-958x632.webp", name: "Hair Oil", price: 11.4, location: "Nijmegen" },
+    { id: 1, photo: "https://www.helium10.com/app/uploads/2023/08/shutterstock_2251573229-copy-958x632.webp", name: "Hair Oil", price: 11.4, location: "Nijmegen",},
     { id: 2, photo: "https://cdn.pixabay.com/photo/2022/03/17/04/57/cosmetic-products-7073743_640.jpg", name: "Conditioner", price: 10, location: "Maastricht" },
     { id: 3, photo: "https://cdn.pixabay.com/photo/2018/08/29/14/47/perfume-3640056_640.jpg", name: "Perfume", price: 98.99, location: "Utrecht" },
     { id: 4, photo: "https://cdn.pixabay.com/photo/2020/09/23/20/27/headphones-5596987_640.jpg", name: "Headphones", price: 114, location: "Almere" },
@@ -24,7 +23,6 @@ const Test: FC = () => {
     { id: 6, photo: "https://cdn.pixabay.com/photo/2020/09/17/06/20/nike-5578104_640.jpg", name: "Sneakers", price: 144, location: "Amsterdam" },
     { id: 7, photo: "https://cdn.pixabay.com/photo/2019/09/03/10/53/watch-4449152_640.jpg", name: "Watch", price: 540, location: "Nijmegen" },
   ];
-
   
   const chunkArray = (arr: any[], size: number) => {
     return arr.reduce((acc, _, i) => {
@@ -57,6 +55,7 @@ const Test: FC = () => {
                     name={product.name}
                     price={product.price}
                     location={product.location}
+                    onClick={cardClicked}
                   />
                 </div>
               ))}
