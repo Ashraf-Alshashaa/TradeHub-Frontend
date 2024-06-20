f47a4ce0321b042b317bedf2e9072c7826d0f235
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-06-20 11:44:20 +0200

    Connected products page to the backend

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,29 +1,40 @@
-import { FC } from 'react';
-import FilterBy from '../../components/filter-by/Filter-by';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
-import ProductCard from '../../components/product-card/Product-card';
-import './products.css';
+import { FC, useEffect } from "react";
+import FilterBy from "../../components/filter-by/Filter-by";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import ProductCard from "../../components/product-card/Product-card";
+import "./products.css";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../../features/products/productsSlice";
+import { RootState } from "../../app/store";
 
 const Test: FC = () => {
+  interface ProductProps {
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    price: number;
+    date: Date;
+    condition: string;
+    category_id: number;
+  }
   const handlePriceChange = (value: [number, number]) => {
-    console.log('Price range changed:', value);
+    console.log("Price range changed:", value);
   };
-  const cardClicked = () => alert("Card clicked")
-  const categories = ['Electronics', 'Furniture', 'Toys', 'Clothes'];
+  const cardClicked = () => alert("Card clicked");
+  const categories = ["Electronics", "Furniture", "Toys", "Clothes"];
 
-  // Example product data
-  const products = [
-    { id: 1, photo: "https://www.helium10.com/app/uploads/2023/08/shutterstock_2251573229-copy-958x632.webp", name: "Hair Oil", price: 11.4, location: "Nijmegen",},
-    { id: 2, photo: "https://cdn.pixabay.com/photo/2022/03/17/04/57/cosmetic-products-7073743_640.jpg", name: "Conditioner", price: 10, location: "Maastricht" },
-    { id: 3, photo: "https://cdn.pixabay.com/photo/2018/08/29/14/47/perfume-3640056_640.jpg", name: "Perfume", price: 98.99, location: "Utrecht" },
-    { id: 4, photo: "https://cdn.pixabay.com/photo/2020/09/23/20/27/headphones-5596987_640.jpg", name: "Headphones", price: 114, location: "Almere" },
-    { id: 5, photo: "https://cdn.pixabay.com/photo/2023/11/10/10/19/cheese-8379082_640.jpg", name: "Grater", price: 14, location: "Eindhoven" },
-    { id: 6, photo: "https://cdn.pixabay.com/photo/2020/09/17/06/20/nike-5578104_640.jpg", name: "Sneakers", price: 144, location: "Amsterdam" },
-    { id: 7, photo: "https://cdn.pixabay.com/photo/2019/09/03/10/53/watch-4449152_640.jpg", name: "Watch", price: 540, location: "Nijmegen" },
-  ];
-  
+  const dispatch = useDispatch();
+  const { products, loading, error } = useSelector(
+    (state: RootState) => state.products
+  );
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   const chunkArray = (arr: any[], size: number) => {
     return arr.reduce((acc, _, i) => {
       if (i % size === 0) {
@@ -36,25 +47,27 @@ const Test: FC = () => {
   // Chunk products into rows of 3 columns each
   const chunkedProducts = chunkArray(products, 3);
 
+  if (loading) return <h1>Loading</h1>;
+  if (error) return <h1>Error</h1>;
+
   return (
-    <div className='Products'>
+    <div className="Products">
       <Header />
-      <div className='row'>
-        <div className='col-3'>
+      <div className="row">
+        <div className="col-3">
           <FilterBy onPriceChange={handlePriceChange} categories={categories} />
         </div>
 
-        <div className='col-9 my-4 overflow-auto scrollable-products'>
-
+        <div className="col-9 my-4 overflow-auto scrollable-products">
           {chunkedProducts.map((row: any, rowIndex: number) => (
-            <div key={rowIndex} className='row mb-5'>
-              {row.map(product => (
-                <div key={product.id} className='col-4 mb-4'>
+            <div key={rowIndex} className="row mb-5">
+              {row.map((product: ProductProps) => (
+                <div key={product.id} className="col-4 mb-4">
                   <ProductCard
-                    photo={product.photo}
+                    photo={product.image}
                     name={product.name}
                     price={product.price}
-                    location={product.location}
+                    location={"Amsterdam"}
                     onClick={cardClicked}
                   />
                 </div>
@@ -63,7 +76,10 @@ const Test: FC = () => {
           ))}
         </div>
       </div>
-      <Footer githubUrl="https://github.com/yourprofile" email="your.email@example.com" />
+      <Footer
+        githubUrl="https://github.com/yourprofile"
+        email="your.email@example.com"
+      />
     </div>
   );
 };
