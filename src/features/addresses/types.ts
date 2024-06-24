3204ede72f644dad29822aaac1b7c2f192dbcac8
Ladan <ladan.rb@gmail.com>
2024-06-24 11:29:16 +0200

    created address Slice with fetch thunk methods.

---
+++ src/features/addresses/types.ts
@@ -0,0 +1,17 @@
+export interface Address {
+    id: number;
+    street_name: string;
+    city: string;
+    country: string;
+    postcode: string;
+    house_number: number;
+    default: boolean;
+}
+
+
+export interface AddressState {
+    all_addresses: Address [];
+    address: Address | null;
+    loading: boolean;
+    error: string | null;
+  }
