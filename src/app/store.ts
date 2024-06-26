ad4e485196a9dc3c7292a542a8cd67dc2f017602
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-06-26 14:44:03 +0200

    Added bids Reduser to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import productReducer from '../features/products/productsSlice';
 import userReducer from '../features/users/userSlice';
+import bidReduser from '../features/bids/bidSlice'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
     users: userReducer,
+    bids: bidReduser,
   },
 });
 
