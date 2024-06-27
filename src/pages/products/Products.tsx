19df2f599fd19efc4747572739066a94afa1b2eb
Ladan <ladan.rb@gmail.com>
2024-06-27 14:11:25 +0200

    Implemented the category filter  in Products page.

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,15 +1,16 @@
 import { FC, useEffect, useState } from "react";
-import FilterBy from "../../components/filter-by/Filter-by";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./products.css";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import FilterBy from "../../components/filter-by/Filter-by";
 import ProductCard from "../../components/product-card/Product-card";
-import "./products.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../features/products/productsSlice";
 import { fetchPriceRange } from "../../features/pricerange/priceRangeSlice";
-import { AppDispatch, RootState } from "../../app/store";
 import { fetchCategories } from "../../features/categories/categorySlice";
+import { RootState } from "../../app/store";
+import { Product } from "../../features/products/types";
+import { AppDispatch } from "../../app/store";
 
 const Test: FC = () => {
 
@@ -20,6 +21,7 @@ const Test: FC = () => {
   const { min_price, max_price } = useSelector(
     (state: RootState) => state.pricerange
   );
+
   const { categories } = useSelector( (state: RootState) => state.categories)
 
 
@@ -27,32 +29,27 @@ const Test: FC = () => {
     setPriceRange([values[0], values[1]]);
   };
 
+
+  const [categoryId, setCategoryId] = useState<(number)>();
+
+  const handleCategoryChange = (id: number) => {
+    setCategoryId(id);
+  };
+
   useEffect(() => {
     dispatch(fetchPriceRange());
     dispatch(fetchCategories())
   }, [dispatch]);
   const [priceRange, setPriceRange] = useState<[number, number]>([min_price, max_price]);
-  
+
+
   useEffect(() => {
     dispatch(fetchProducts({
       min_price: priceRange[0],
-      max_price: priceRange[1]
+      max_price: priceRange[1],
+      category_id: categoryId,
     }));
-  }, [dispatch, priceRange]);
-
-  
-console.log(products)
-  interface ProductProps {
-    id: number;
-    name: string;
-    image: string;
-    description: string;
-    price: number;
-    date: Date;
-    condition: string;
-    category_id: number;
-  }
-
+  }, [dispatch, priceRange, categoryId]);
 
   const chunkArray = (arr: any[], size: number) => {
     return arr.reduce((acc, _, i) => {
@@ -63,9 +60,6 @@ console.log(products)
     }, []);
   };
 
-  const category_names: string[] = categories.map(category => category.name);
-
-  // Chunk products into rows of 3 columns each
   const chunkedProducts = chunkArray(products, 3);
 
   // if (loading) return <h1>Loading</h1>;
@@ -76,13 +70,13 @@ console.log(products)
       <Header />
       <div className="row">
         <div className="col-3">
-        <FilterBy categories={category_names} priceRange={priceRange} onPriceChange={handlePriceChange} />
+        <FilterBy categories={categories} priceRange={priceRange} onPriceChange={handlePriceChange} onCategoryChange={handleCategoryChange}/>
         </div>
 
         <div className="col-9 my-4 overflow-auto scrollable-products">
           {chunkedProducts.map((row: any, rowIndex: number) => (
             <div key={rowIndex} className="row mb-5">
-              {row.map((product: ProductProps) => (
+              {row.map((product: Product) => (
                 <div key={product.id} className="col-4 mb-4">
                   <ProductCard
                     photo={product.image}
