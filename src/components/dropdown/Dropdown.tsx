c24d648b3f7b192fba2bb7585359459415cffec3
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-06-27 11:22:20 +0200

    Added selector prop to dropdwon to change the title after choosing item

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,9 +1,15 @@
-import { FC } from "react";
+import { FC, ReactNode, useState } from "react";
 import { DropDownMenuProps } from "./types";
 import { Dropdown } from "react-bootstrap";
 import "./styles.css";
 
-const DropdownMenu: FC<DropDownMenuProps> = ({ title, data, variant }) => {
+const DropdownMenu: FC<DropDownMenuProps> = ({
+  title,
+  data,
+  variant,
+  selector = false,
+}) => {
+  const [dropdownTitle, setDropdownTitle] = useState<string | ReactNode>(title);
   return (
     <Dropdown className="w-100 h-100">
       <Dropdown.Toggle
@@ -11,14 +17,18 @@ const DropdownMenu: FC<DropDownMenuProps> = ({ title, data, variant }) => {
         variant={variant ? variant : "light"}
         id="dropdown-basic"
       >
-        <span className="dropdown-title">{title}</span>
+        <span className="dropdown-title">{dropdownTitle}</span>
       </Dropdown.Toggle>
       <Dropdown.Menu className="w-100">
         {data.map(({ id, onClick, content }) => (
           <Dropdown.Item
             key={"dropdown-" + "title-" + id}
             id={id}
-            onClick={onClick}
+            onClick={() => {
+              onClick();
+
+              selector && setDropdownTitle(content);
+            }}
             className="dropdown-item"
           >
             {content}
