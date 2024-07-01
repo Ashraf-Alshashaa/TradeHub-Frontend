4d70d8b03a8b150151feedfb63c756c6f220d483
gaidds <ad.gaidamovich@gmail.com>
2024-07-01 15:08:44 +0200

    price range doesn't change after refresh

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -57,16 +57,13 @@ useEffect(() => {
   const urlParams = new URLSearchParams(location.search);
   const search = urlParams.get("search") || "";
   const category = urlParams.get("category") || null;
-  const minPrice = parseInt(urlParams.get("min_price") || min_price.toString());
-  const maxPrice = parseInt(urlParams.get("max_price") || max_price.toString());
-
 
   setSearchQuery(search);
   setCategoryId(category ? parseInt(category) : null);
-  setPriceRange([minPrice, maxPrice]);
+  setPriceRange([parseInt(urlParams.get("min_price") || min_price.toString()), parseInt(urlParams.get("max_price") || max_price.toString())]);
 
-  fetchFilteredProducts(search, category ? parseInt(category) : null, [minPrice, maxPrice]);
-}, [location.search, min_price, max_price]);
+  fetchFilteredProducts(search, category ? parseInt(category) : null, priceRange);
+}, [location.search]);
 
 
 
