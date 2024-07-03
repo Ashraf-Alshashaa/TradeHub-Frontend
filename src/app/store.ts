9d76c62a0c6d2bd90ecd7c52fa4a565aa1c66197
Ladan <ladan.rb@gmail.com>
2024-07-03 15:26:06 +0200

    added notificationSlice and webSocketMiddleware to store.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,15 @@
+// src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import productReducer from '../features/products/productsSlice';
 import userReducer from '../features/users/userSlice';
-import bidReduser from '../features/bids/bidSlice'
-import addressReducer from '../features/addresses/addressSlice'
+import bidReducer from '../features/bids/bidSlice';
+import addressReducer from '../features/addresses/addressSlice';
 import priceRangeReducer from '../features/pricerange/priceRangeSlice';
 import categoryReducer from '../features/categories/categorySlice';
 import paymentReducer from '../features/payments/paymentSlice';
-import notificationReducer from '../features/notification/notificationSlice'
+import notificationReducer from '../features/notification/notificationSlice';
+import websocketMiddleware from '../features/websocket/websocket';
 
 const store = configureStore({
   reducer: {
@@ -21,6 +23,7 @@ const store = configureStore({
     payments: paymentReducer,
     notifications: notificationReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(websocketMiddleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
